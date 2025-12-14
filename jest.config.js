dcd83611d240e507f8e901ced639cf5ce9184c8f
nadavramon <ramonn3@yahoo.com>
2025-12-14 18:02:28 +0200

    fix(test): add babel config and switch to jest-expo preset

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  preset: "react-native",
+  preset: "jest-expo",
   testEnvironment: "node",
-  transformIgnorePatterns: [
-    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
-  ],
 };
