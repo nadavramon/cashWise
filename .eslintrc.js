7dda3e3af428ddd9758151c40edf4566971e03c8
nadavramon <ramonn3@yahoo.com>
2025-12-14 18:19:52 +0200

    ci: Adjust ESLint ignore patterns, remove CI Metro cache, and update Jest config.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,9 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: ["expo", "prettier"],
-  plugins: ["prettier"],
-  rules: {
-    "prettier/prettier": "error",
-  },
+    extends: ["expo", "prettier"],
+    plugins: ["prettier"],
+    rules: {
+        "prettier/prettier": "error",
+    },
+    ignorePatterns: ["node_modules/", "dist/", "build/", ".expo/", "coverage/"],
 };
--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,3 @@
 module.exports = {
   preset: "jest-expo",
-  testEnvironment: "node",
 };
